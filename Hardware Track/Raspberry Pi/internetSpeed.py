ee3cd3b917d0e84fd735be4ba8c974fa93e84c7e
Haripriya <haripriya01212@gmail.com>
2024-09-24 18:21:35 +0530

    added r pi question

---
+++ Hardware Track/Raspberry Pi/internetSpeed.py
@@ -0,0 +1,7 @@
+'''
+    Internet Speed Monitor
+    Use your Raspberry Pi to monitor your internet connection speed. 
+    You can set up a Python script to automatically run speed tests at regular intervals and log the results for tracking your ISP’s performance.
+'''
+
+# WRITE YOUR CODE HERE
