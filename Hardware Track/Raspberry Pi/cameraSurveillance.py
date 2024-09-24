78b70a018228ad5e5412647f8af9ec3fd739985e
Haripriya <haripriya01212@gmail.com>
2024-09-24 18:03:54 +0530

    added r pi question

---
+++ Hardware Track/Raspberry Pi/cameraSurveillance.py
@@ -0,0 +1,5 @@
+'''
+    Pi Camera Surveillance System
+    Set up a simple surveillance system using the Raspberry Pi Camera Module. 
+    The system should capture an image or video whenever motion is detected and save it to the Pi's storage.
+'''
