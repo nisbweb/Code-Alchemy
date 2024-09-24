b2ebe07640f35fe0a53e4edf519bac1509258ae7
Haripriya <haripriya01212@gmail.com>
2024-09-24 11:09:03 +0530

    changes in q1

--- Software Track/Level 1: The Memory Vault/string challenge.cpp
+++ Software Track/Level 1: The Memory Vault/string challenge.cpp
@@ -1,15 +1,17 @@
 /*
-    1. **Array/String Challenge:** The first task is to reorganize the fragmented data blocks using array and
+    1. Array/String Challenge: The first task is to reorganize the fragmented data blocks using array and
     string manipulation. You need to scan through the memory sectors and arrange them in the correct
     order to ensure efficient data retrieval.
 
-    Each memory sector contains a block of data represented as a string. However, the data blocks have been fragmented and shuffled out of order. Your task is to rearrange these blocks by sorting them in ascending order based on their memory sector numbers.	
+    Each memory sector contains a block of data represented as a string. However, the data blocks have been fragmented and shuffled out of order. 
+    Your task is to rearrange these blocks by sorting them in ascending order based on their memory sector numbers.	
     Input : 
     integer n representing number of fragments/data. blocks.          
-    An array of n strings where each string is a memory sector. Each memory sector is prefixed by an integer (sector number) followed by the actual data. The sector number and the data are separated by a colon (`:`).
+    An array of n strings where each string is a memory sector. Each memory sector is prefixed by an integer (sector number) followed by the actual data. 
+    The sector number and the data are separated by a colon (`:`).
 
     Output :
-    print the correctly ordered array (not just printing in order, the list should also be in order)
+    print the correctly ordered array (not just printing in order, the array should also be in order)
 
     Example:
 
