f8bcc9681e88e27dc141cfb0f57612be6db144e6
Haripriya <haripriya01212@gmail.com>
2024-09-24 15:38:12 +0530

    added q6

---
+++ Software Track/Level 1: The Memory Vault/dataTransfer.cpp
@@ -0,0 +1,23 @@
+/*
+    You are needed to transfer important data. So your goal is to maximize the amount of data transferred. 
+    Given a list of memory block sizes and a maximum capacity, choose the blocks to maximize the transferred data while not exceeding the capacity.
+
+    Input:
+    A list of integers representing memory block sizes.
+    An integer capacity, the maximum size that can be transferred.
+
+    Output:
+    An integer representing the maximum sum of memory block sizes that can be transferred.
+
+    Example
+    Input
+    memory_blocks = [2, 3, 5, 7, 1]
+    capacity = 10
+
+    Output
+    10
+
+    [You can transfer blocks of size 2, 3, and 5 for a total of 10]
+*/
+
+// WRITE YOUR CODE HERE
