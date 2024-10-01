e951edf5420a5c109e12dbcf1ef9a30087745036
Mridul Bhardwaj <mridul240205@gmail.com>
2024-10-01 19:32:37 +0530

    Insertion Sort implementation using C++

--- DSA practice/insertionSort.cpp
+++ DSA practice/insertionSort.cpp
@@ -1,1 +1,25 @@
-// WRITE YOUR CODE HERE
+#include<bits/stdc++.h>
+using namespace std;
+
+void insertionSort(int arr[], int n){
+    for(int start=0;start<n;start++){
+        int min_index = start;
+        for(int i=start+1;i<n;i++){
+            if(arr[i]<arr[min_index]){
+                min_index = i;
+            }
+        }
+        swap(arr[start], arr[min_index]);
+    }
+}
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i=0; i<n ; i++){
+        cin >> arr[i];
+    }
+
+    insertionSort(arr, n);
+}
