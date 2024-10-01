2596ed87c54e552a90c816664d9eae06b5f4e939
sentieant <aravindsn002@gmail.com>
2024-10-01 12:21:12 +0530

    binary search implementation using c++

--- DSA practice/binarySearch.cpp
+++ DSA practice/binarySearch.cpp
@@ -1,2 +1,34 @@
-// WRITE YOUR CODE HERE
-// Hello
+#include <bits/stdc++.h>
+using namespace std;
+
+int binarySearch(int arr[], int n, int target) {
+    int left = 0, right = n - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == target)
+            return mid;
+
+        if (arr[mid] < target)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+
+    return -1;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    int target;
+    cin >> target;
+
+    for (int i=0; i<n ; i++){
+        cin >> arr[i];
+    }
+
+    binarySearch(arr, n, target);
+}
