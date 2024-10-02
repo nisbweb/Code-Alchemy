e14d2be6777f1f52aa42b70403c2424da9cacfbd
bishalshaw-164 <75210630+bishalshaw-164@users.noreply.github.com>
2024-10-02 19:45:29 +0530

    Update palindromeNumber.cpp with solution (#6)
    Right code. Merged your solution

--- Puzzles Track/palindromeNumber.cpp
+++ Puzzles Track/palindromeNumber.cpp
@@ -16,4 +16,32 @@
 */
 
 // WRITE YOUR CODE HERE
+#include <iostream>
+using namespace std;
+
+bool isPalindrome(int num) {
+    int originalNum = num;
+    int reversedNum = 0;
+    while (num > 0) {
+        int digit = num % 10;
+        reversedNum = reversedNum * 10 + digit;
+        num /= 10;
+    }
+    return originalNum == reversedNum;
+}
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (isPalindrome(num)) {
+        cout << num << " is a palindrome." << endl;
+    } else {
+        cout << num << " is not a palindrome." << endl;
+    }
+
+    return 0;
+}
+
 
