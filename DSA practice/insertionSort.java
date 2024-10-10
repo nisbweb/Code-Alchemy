3c753d7bc606133ed5833aa236bcffa2c8badd9f
Pranav A Korlahalli <158452172+pranav-326@users.noreply.github.com>
2024-10-10 23:27:52 +0530

    Solved DSA problems using JAVA (#14)
    * added DSA
    
    I have used JAVA to solve Lsearch and I sort
    
    * Updated insertionSort.java
    
    added Arrays.toString

---
+++ DSA practice/insertionSort.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class insertionSort {
+    public static int[] isort(int[] n) {
+        int temp,j;
+        for (int i = 0; i < n.length; i++) {
+            j=i;
+            while (j>=1) {
+                if (n[j]<n[j-1]) {
+                    temp=n[j];
+                    n[j]=n[j-1];
+                    n[j-1]=temp;
+                }
+                j--;
+            }
+        }
+        return n;
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the number of elements:");
+        int n = in.nextInt();
+        int[] arr=new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]=in.nextInt();
+        }
+        System.out.println("Sorted array: "+ Arrays.toString(isort(arr)));
+    }
+}
