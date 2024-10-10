e27b9a340ffd3d59dff171a3aad8d021e315a0b0
Anmol Pathak <anmolpathak022@gmail.com>
2024-10-10 21:53:52 +0530

    linearSearch

--- DSA practice/linearSearch.cpp
+++ DSA practice/linearSearch.cpp
@@ -1,1 +1,26 @@
-// WRITE YOUR CODE HERE
+#include <bits/stdc++.h>
+using namespace std;
+
+int linearSearch(int arr[], int n, int target) {
+    
+    for (int i = 0; i < n; i++)
+    {
+        if(arr[i]==target){
+            return i;
+        }
+    }
+
+    return -1;
+    
+    
+}
+
+int main() {
+    int arr[]= {1,5,2,53,62,2,7,3,8,4,52,6};
+  
+    int target=8;
+  
+    cout<<linearSearch(arr, 12, 6);
+
+    return 0;
+}
