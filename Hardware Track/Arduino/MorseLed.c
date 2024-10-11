02a6acd450fe9a9df0e7b5c5ca3108bd25bb86fe
Anmol Pathak <anmolpathak022@gmail.com>
2024-10-11 11:26:54 +0530

    morseLed

--- Hardware Track/Arduino/MorseLed.c
+++ Hardware Track/Arduino/MorseLed.c
@@ -11,4 +11,74 @@
 
 */
 
-// WRITE YOUR CODE HERE
+/*
+solution:
+
+the standard morse rules were followed, 
+the unit of measurement is= 500ms
+dot=1 unit
+dash=3 unit
+delay b/w dots and dashes =1 unit
+delay b/w diffrent letters=3 units
+
+*/
+
+//might improve the code in future, but i am done for now... 
+
+
+const int led=13;
+
+void setup(){
+
+    pinMode(led, OUTPUT);
+
+}
+
+void loop(){
+char message[]="ANMOL";
+
+for (int i = 0; message[i] != '\0' ; i++)
+{
+    toMorse(message[i]);
+ 	delay(1500);
+}
+
+
+
+}
+
+const char *morse[]={
+ ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", 
+  "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", 
+  "..-", "...-", ".--", "-..-", "-.--", "--..", // A-Z
+};
+
+void toMorse(char A){
+
+    const char *Ac=morse[A-'A'] ;
+
+     while (*Ac) {
+      if (*Ac == '.') {
+        Dot();
+      } else if (*Ac == '-') {
+        Dash();
+      }
+      Ac++;
+		delay(500);
+    }
+    delay(1500);
+}
+
+void Dot(){
+    digitalWrite(led, HIGH);
+  delay(500);
+  digitalWrite(led, LOW);
+  delay(500); 
+}
+
+void Dash() {
+  digitalWrite(led, HIGH);
+  delay(1500);
+  digitalWrite(led, LOW);
+  delay(1500); // Space between symbols
+}
