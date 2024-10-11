391afe9764aab405bdfa8bbd833d8b913209aadc
Kimaya Gupta <91895421+Kimayaa1@users.noreply.github.com>
2024-10-11 14:47:49 +0530

    Solving Book Allocator Problem.cpp

---
+++ DSA practice/Book Allocator Problem.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+
+bool isPossible(int arr[], int n, int m, int mid){
+
+    int studentCount = 1;
+    int pageSum = 0;
+
+    for(int i =0; i<n ; i++){
+        if(pageSum + arr[i] <= mid){
+            pageSum += arr[i];
+        }
+        else{
+            studentCount++;
+            if(studentCount > m || arr[i]>mid){
+                return false;
+            }
+            pageSum += arr[i];
+        }
+    }
+    return true;
+}
+
+int BookAllocator(int arr[], int n, int m){
+
+    int s = 0;
+    int sum = 0;
+    
+    for(int i =0; i<n; i++){
+        sum += arr[i];
+    }
+    int e = sum;
+    int ans = -1;
+    int mid = s + (e-s)/2;
+
+    while(s<=e){
+
+        if(isPossible(arr, n, m, mid)){
+            ans = mid;
+            e = mid - 1;
+        }
+        else{
+            s = mid+1 ;
+        }
+
+        mid = s + (e-s)/2;
+    }
+    return ans;
+}
+
+int main(){
+
+    int arr[4] = {10,20,30,40};
+    cout << BookAllocator(arr, 4, 2);
+}
