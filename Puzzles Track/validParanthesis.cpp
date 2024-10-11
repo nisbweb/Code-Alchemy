58bbf9355a3b90b39a8ee58831e336f83bb1b62b
Lavanya Lenka <78673575+Lavanyasuc31@users.noreply.github.com>
2024-10-11 10:42:17 +0530

    added solution for validParanthesis in puzzle track (#10)

--- Puzzles Track/validParanthesis.cpp
+++ Puzzles Track/validParanthesis.cpp
@@ -25,3 +25,58 @@
 */
 
 // WRITE YOUR CODE HERE
+
+
+
+ bool isValid(string s) {
+       stack <char> container;
+       for (int i=0;i<s.size();i++)
+       {
+        if((s[i]=='[') || (s[i]== '{') || (s[i]=='(') ){
+            container.push(s[i]);
+        }
+        else{
+            if((s[i]==')') && (!container.empty())){
+                char c = container.top();
+                if(c!='(')
+                    return false;
+                else
+                    container.pop();
+            }
+            else if((s[i]=='}') && (!container.empty())){
+                char c = container.top();
+                if(c!='{')
+                    return false;
+                else
+                    container.pop();
+            }
+            else if((s[i]==']') && (!container.empty())){
+                char c = container.top();
+                if(c!='[')
+                    return false;
+                else
+                    container.pop();
+            }
+            else
+                return false;
+        }
+       } 
+       if (container.empty())
+        return true;
+       return false;
+        
+    }
+
+
+int main()
+{
+	string s;
+	cout<<"Enter the string";
+	cin>>s;
+	bool ans = isValid(s);
+	if(ans)
+		cout<<"It is a valid paranthesis";
+	else
+		cout<<"It is not a valid paranthesis";
+	return 0;
+}
