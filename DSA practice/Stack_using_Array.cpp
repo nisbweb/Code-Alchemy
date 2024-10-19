c3e6ce9396aa7b5b73356bc91fe883436149a18f
Shubham Jain <168662207+shubhamjain1402@users.noreply.github.com>
2024-10-20 00:12:50 +0530

    Update Stack_using_Array.cpp

--- DSA practice/Stack_using_Array.cpp
+++ DSA practice/Stack_using_Array.cpp
@@ -13,14 +13,26 @@ class Stack {
     }
   void push(int x) {
     top++;
+    if (top >= size - 1) { 
+            cout << "Stack Overflow! Cannot push " << x << endl;
+            return;
+    }
     arr[top] = x;
   }
   int pop() {
+    if (top < 0) { 
+            cout << "Stack Underflow! Cannot pop from an empty stack." << endl;
+            return -1;
+    }
     int x = arr[top];
     top--;
     return x;
   }
   int Top() {
+    if (top < 0) {
+            cout << "Stack is empty! No top element." << endl;
+            return -1;
+        }
     return arr[top];
   }
   int Size() {
